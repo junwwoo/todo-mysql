7325feab2e31e8db1f527ac27a7e069d0bd6cca2
junwoo <wnsdn5582@gmail.com>
2025-12-29 01:43:58 +0900

    Test 클래스 구현

--- src/test/java/com/asdf/todo/controller/TodoControllerTests.java
+++ src/test/java/com/asdf/todo/controller/TodoControllerTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,7 +27,7 @@ public class TodoControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TodoService todoService;
 
     @Test
